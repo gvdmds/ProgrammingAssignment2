47a96c7f7cf23545310c33405b16ca758f9f73b9
Guy Van der Meeren <Guy Van der Meeren>
2016-12-02 20:52:53 +0100

    gvdm RP2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## testmatrix: mat <- matrix(rnorm(25),nrow=5,ncol=5)
 
-## Write a short comment describing this function
+
+
+## create an object to hold a matrix, its inverse and varia set/get functions
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## 1) initialize
+	inv <- NULL
+	## x is initialized in function call	
+
+## 2) store
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+		
+## 3) retrieve
+	get <- function() x
+
+## 4) store inverted matrix
+	setinv <- function(y) inv <<- y
+
+## 5) retrieve inverted matrix
+	getinv <- function() inv
+
+## 6 make functions available
+list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## take inverse of matrix, first check if available in cache, if not calculate
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+
+## get inverse from cache
+	s <- x$getinv()
+
+## check if inverse exist and if not calculate it
+	if (!is.null(s)) {
+	print("getting inverse from cache")
+	return(s)
+	}
+	else { 
+	print("calculating inverse")
+		y <- x$get()
+		s <- solve(y)
+		x$setinv(s)
+		s
+	}
 }
+
+
+
+
+
+
+
+
